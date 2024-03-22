70adfc3a91dc6ff9bcf4605393fe5220be7108ea
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-03-22 19:33:18 +0200

    wdwdwdwd

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const input = document.querySelector(".input");
-const btn = document.querySelector(".btn");
-
-btn.addEventListener("click", findLock)
+let input = document.querySelector(".input");
+let btn = document.querySelector(".btn");
+let listkountri = document.querySelector(".kountri");
+btn.addEventListener("click", findLock);
 
 function findLock() {
-  URLL =  `https://restcountries.com/v3.1/name/${input.value}` 
-  return URLL;
+  let cou8ntruName = input.value;
+  let myURL = `https://restcountries.com/v3.1/name/${cou8ntruName}`;
+  console.log(myURL);
+  fetch(myURL)
+    .then((data) => data.json())
+    .then((data) => {
+      const listMarkUp = data.map((data) => {
+        console.log(data);
+        const listElem = `
+  <div class="conteiner2">
+   <img src="${data.flags.svg}" class="flagImg">
+        <h2>${data.name.common}</h2>
+        <div class="conteiner">
+            <div class="data-conteiner">
+                <h4>заходиться на континенті:</h4>
+                <span>${data.continents}</span>
+            </div>
+        </div>
+         <div class="conteiner">
+            <div class="data-conteiner">
+                <h4>Людей:</h4>
+                <span>${data.population}</span>
+            </div>
+        </div>
+        </div>
+         <div class="conteiner">
+            <div class="data-conteiner">
+                <h4>МОва:</h4>
+                <span>${data.languages[Object.keys(data.languages)]}
+                  </span>
+            </div>
+        </div>
+  </div>
+      `;
+        return listElem;
+      });
+      listkountri.innerHTML = listMarkUp;
+    });
 }
-
-const listkountri = document.querySelector(".kountri")
-const URLL = `https://restcountries.com/v3.1/name/uk`
-
-function fetchUsers(url) {
-    
-const data = fetch(url)
-console.log(data)
-return data;
-}
-
-fetchUsers(URLL).then((data) => data.json()).then((data) => createUserMapArt(data)).catch((error) => console.log(error))
-
-
-function createUserMapArt(data) {
-    console.log(data)
- const listMarkUp = data.map((user) => {
-   const listElem = `
-   <li >
-   <img src="${user.flags.png}" alt="${user.type}">
-   <p class="namelog">Назва:${user.name.common}</p>
-   <p class="popuration">Популяція:${user.population}лудей</p>
-   </li>`;
-return listElem;
-  })
-  listkountri.innerHTML = listMarkUp;
-   
-  
-}
-
-
-
-
