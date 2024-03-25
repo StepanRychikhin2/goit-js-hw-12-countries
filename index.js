8138bcf768b84e4d00d548734b83eeb32b0a6c99
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-03-25 18:10:56 +0200

    rsrtjsrjty

--- index.js
+++ index.js
@@ -2,55 +2,65 @@ let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
 let listkountri = document.querySelector(".kountri");
 btn.addEventListener("click", findLock);
-
+let arr = [];
 function findLock() {
   let cou8ntruName = input.value;
   let myURL = `https://restcountries.com/v3.1/name/${cou8ntruName}`;
   console.log(myURL);
   fetch(myURL)
-.then((data) => data.json())
-.then((data) => {
+    .then((data) => data.json())
+    .then((data) => {
       const listMarkUp = data.map((data) => {
-        console.log(data);
-        const listElem = `
-        <li class="bb">
-  <div class="conteiner2">
-   <img src="${data.flags.svg}" class="flagImg">
-        <h2>${data.name.common}</h2>
-        <div class="conteiner">
-            <div class="data-conteiner">
-                <h4>заходиться на континенті:</h4>
-                <span>${data.continents}</span>
-            </div>
-        </div>
-         <div class="conteiner">
-            <div class="data-conteiner">
-                <h4>Людей:</h4>
-                <span>${data.population}</span>
-            </div>
-        </div>
-        </div>
-         <div class="conteiner">
-            <div class="data-conteiner">
-                <h4>МОва:</h4>
-                <span>${data.languages[Object.keys(data.languages)]}
-                  </span>
-            </div>
-        </div>
-  </div>
-  </li>
-      `;
-        return listElem;
-      });
-      listkountri.innerHTML = listMarkUp;
-      })
-      .catch(() => {
-        if (cou8ntruName == 0) {
-          alert("Введіть коктне значеня")
-        } else {
-          alert("Введіть коктне значеня 22")
-        }
-     
+      if (arr.length == 1) {
+        
+          console.log(data);
+          
+          const listElem = `
+      <li class="bb">
+<div class="conteiner2">
+ <img src="${data.flags.svg}" class="flagImg">
+      <h2>${data.name.common}</h2>
+      <div class="conteiner">
+          <div class="data-conteiner">
+              <h4>заходиться на континенті:</h4>
+              <span>${data.continents}</span>
+          </div>
+      </div>
+       <div class="conteiner">
+          <div class="data-conteiner">
+              <h4>Людей:</h4>
+              <span>${data.population}</span>
+          </div>
+      </div>
+      </div>
+       <div class="conteiner">
+          <div class="data-conteiner">
+              <h4>МОва:</h4>
+              <span>${data.languages[Object.keys(data.languages)]}
+                </span>
+          </div>
+      </div>
+</div>
+</li>
+    `;
+          return listElem;
+       
+       
+      } else if (arr.length == 0) {
+        arr.push(data.name.common);
+        console.log(arr);
+      } else {
+        console.log(arr);
+      }
+
     });
-   
+    listkountri.innerHTML = listMarkUp;
+    })
+    .catch(() => {
+      if (cou8ntruName == 0) {
+        alert("Введіть коктне значеня");
+      } else {
+        alert("Введіть коктне значеня 22");
+      }
+    });
 }
